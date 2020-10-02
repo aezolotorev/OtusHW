79ae9f38fcc806f0cb1a47d8b877327f14a9a39f
aezolotorev <aezolotorev@mail.ru>
2020-10-02 19:08:44 +0300

    1 HW
    1 HW

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public enum State
+    {
+        Idle,
+        RunningToEnemy,
+        RunningFromEnemy,
+        BeginAttack,
+        Attack,
+        BeginShoot,
+        Shoot,
+        Die,
+    }
+
+    public enum Weapon
+    {
+        Pistol,
+        Bat,
+        Fist,
+    }
+
+    Animator animator;
+    State state;
+
+    public Weapon weapon;
+    public Transform target;
+    public float runSpeed;
+    public float distanceFromEnemy;
+    Vector3 originalPosition;
+    Quaternion originalRotation;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        state = State.Idle;
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+    }
+
+    public void SetState(State newState)
+    {
+        state = newState;
+    }
+
+    [ContextMenu("Attack")]
+    void AttackEnemy()
+    {
+        switch (weapon) {
+            case Weapon.Bat:
+                state = State.RunningToEnemy;
+                break;
+            case Weapon.Fist:
+                state = State.RunningToEnemy;
+                break;
+            case Weapon.Pistol:
+                state = State.BeginShoot;
+                break;
+        }
+    }
+
+    bool RunTowards(Vector3 targetPosition, float distanceFromTarget)
+    {
+        Vector3 distance = targetPosition - transform.position;
+        if (distance.magnitude < 0.00001f) {
+            transform.position = targetPosition;
+            return true;
+        }
+
+        Vector3 direction = distance.normalized;
+        transform.rotation = Quaternion.LookRotation(direction);
+
+        targetPosition -= direction * distanceFromTarget;
+        distance = (targetPosition - transform.position);
+
+        Vector3 step = direction * runSpeed;
+        if (step.magnitude < distance.magnitude) {
+            transform.position += step;
+            return false;
+        }
+
+        transform.position = targetPosition;
+        return true;
+    }
+
+    void FixedUpdate()
+    {
+        switch (state) {
+            case State.Idle:
+                transform.rotation = originalRotation;
+                animator.SetFloat("Speed", 0.0f);
+                break;
+
+            case State.RunningToEnemy:
+                animator.SetFloat("Speed", runSpeed);
+                if (RunTowards(target.position, distanceFromEnemy))
+                    state = State.BeginAttack;
+                break;
+
+            case State.RunningFromEnemy:
+                animator.SetFloat("Speed", runSpeed);
+                if (RunTowards(originalPosition, 0.0f))
+                    state = State.Idle;
+                break;
+
+            case State.BeginAttack:
+                if (weapon==Weapon.Bat)
+                {
+                    animator.SetTrigger("MeleeAttack");
+                    state = State.Attack;
+                }
+                if (weapon == Weapon.Fist)
+                {
+                    animator.SetTrigger("FistAttack");
+                    state = State.Attack;
+                }
+                break;
+
+
+            case State.Attack:
+                break;
+
+            case State.BeginShoot:
+                animator.SetTrigger("Shoot");
+                state = State.Shoot;
+                break;
+
+            case State.Shoot:
+                break;
+
+            case State.Die:
+                animator.SetBool("Dead", true);
+                break;
+        }
+    }
+
+    [ContextMenu("Ray")]//используетсядля отладки
+    void GiveDamage()
+    {
+        
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            
+            GameObject hitobject = hit.transform.gameObject;
+            Debug.Log(hitobject.name);
+            if (hitobject.GetComponent<Character>())
+            {
+                Debug.Log(hitobject.name);
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/CharacterAnimationEvents.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationEvents : MonoBehaviour
+{
+    Character character;
+    void Start()
+    {
+        character = GetComponentInParent<Character>();
+    }
+
+    void ShootEnd()
+    {
+        character.SetState(Character.State.Idle);
+    }
+
+    void AttackEnd()
+    {
+        character.SetState(Character.State.RunningFromEnemy);
+    }
+
+    void GiveDamage()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if(Physics.Raycast(ray,  out hit))
+        {            
+            GameObject hitobject = hit.transform.gameObject;
+            Debug.Log(hitobject.name);
+            if (hitobject.GetComponentInParent<Character>())
+            {
+                Debug.Log("2");
+                hitobject.GetComponentInParent<Character>().SetState(Character.State.Die);
+            }
+        } 
+
+    }
+  
+    
+
+
+   
+}
